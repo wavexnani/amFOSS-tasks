aeb90e39f157de102ea99d7ad7a461d099a3d1f0
chandramouli <mouli7666@gmail.com>
2025-01-20 10:14:19 +0530

    adding task-03

---
+++ task-03/index.js
@@ -0,0 +1,20 @@
+var audio1 = new Audio("Sounds/crash.mp3");
+audio1.loop = false;
+
+var audio2 = new Audio("Sounds/kick-bass.mp3");
+audio2.loop = false;
+
+var audio3 = new Audio("Sounds/snare.mp3");
+audio3.loop = false;
+
+var audio4 = new Audio("Sounds/tom-1.mp3");
+audio4.loop = false;
+
+var audio5 = new Audio("Sounds/tom-2.mp3");
+audio5.loop = false;
+
+var audio6 = new Audio("Sounds/tom-3.mp3");
+audio6.loop = false;
+
+var audio7 = new Audio("Sounds/tom-4.mp3");
+audio7.loop = false;
